fe5920f3223b8f74b6511f4c5bd4d1858e4ffab1
abc <abc@xyz.com>
2021-06-09 20:50:56 +0530

    fixed issue with imx258.dtb

--- Toybrick_USB.mk
+++ Toybrick_USB.mk
@@ -32,8 +32,8 @@ include toolchain.mk
 ################################################################################
 KERNEL_EXPORTS	?= CROSS_COMPILE="$(CCACHE)$(AARCH64_CROSS_COMPILE)"
 KERNEL_FLAGS	?= ARCH=arm64
-VERSION	?= "2.0"
-DTB		?= rk3399pro-toybrick-prod-linux
+VERSION	= 2.0
+DTB		= rk3399pro-toybrick-prod-linux
 
 
 kernel:
@@ -45,15 +45,13 @@ kernel:
 	$(KERNEL_EXPORTS) $(MAKE) -C $(KERNEL_PATH) $(KERNEL_FLAGS) $(DTB)-edp.img
 	$(KERNEL_EXPORTS) $(MAKE) -C $(KERNEL_PATH) $(KERNEL_FLAGS) $(DTB)-mipi.img
 	$(KERNEL_EXPORTS) $(MAKE) -C $(KERNEL_PATH) $(KERNEL_FLAGS) $(DTB)-u2.img
+	$(KERNEL_EXPORTS) $(MAKE) -C $(KERNEL_PATH) $(KERNEL_FLAGS) $(DTB)-imx258.img
+	$(KERNEL_EXPORTS) $(MAKE) -C $(KERNEL_PATH) $(KERNEL_FLAGS) $(DTB).img
 	
 	cp -f $(KERNEL_PATH)/arch/arm64/boot/dts/rockchip/$(DTB)-u2.dtb $(KERNEL_PATH)/boot_linux/extlinux/toybrick-u2.dtb
 	cp -f $(KERNEL_PATH)/arch/arm64/boot/dts/rockchip/$(DTB)-edp.dtb $(KERNEL_PATH)/boot_linux/extlinux/toybrick-edp.dtb
 	cp -f $(KERNEL_PATH)/arch/arm64/boot/dts/rockchip/$(DTB)-mipi.dtb $(KERNEL_PATH)/boot_linux/extlinux/toybrick-mipi.dtb
 	cp -f $(KERNEL_PATH)/arch/arm64/boot/dts/rockchip/$(DTB)-imx258.dtb $(KERNEL_PATH)/boot_linux/extlinux/toybrick-imx258.dtb
-	
-	$(KERNEL_EXPORTS) $(MAKE) -C $(KERNEL_PATH) $(KERNEL_FLAGS) $(DTB)-imx258.img
-	$(KERNEL_EXPORTS) $(MAKE) -C $(KERNEL_PATH) $(KERNEL_FLAGS) $(DTB).img
-	
 	cp -f $(KERNEL_PATH)/arch/arm64/boot/dts/rockchip/$(DTB).dtb $(KERNEL_PATH)/boot_linux/extlinux/toybrick.dtb
 	cp -f $(KERNEL_PATH)/arch/arm64/boot/dts/rockchip/$(DTB).dtb $(KERNEL_PATH)/boot_linux/extlinux/toybrick-default.dtb
 	cp -f $(KERNEL_PATH)/arch/arm64/boot/Image $(KERNEL_PATH)/boot_linux/extlinux/
@@ -61,9 +59,9 @@ kernel:
 	echo "label rockchip-kernel-4.4" > $(KERNEL_PATH)/boot_linux/extlinux/extlinux.conf
 	echo "	kernel /extlinux/Image" >> $(KERNEL_PATH)/boot_linux/extlinux/extlinux.conf
 	echo "	fdt /extlinux/toybrick.dtb" >> $(KERNEL_PATH)/boot_linux/extlinux/extlinux.conf
-	echo "	append  earlycon=uart8250,mmio32,0xff1a0000 initrd=/initramfs-toybrick-${VERSION}.img root=PARTUUID=614e0000-0000-4b53-8000-1d28000054a9 rw rootwait rootfstype=ext4" >> $(KERNEL_PATH)/boot_linux/extlinux/extlinux.conf
+	echo "	append  earlycon=uart8250,mmio32,0xff1a0000 initrd=/initramfs-toybrick-$(VERSION).img root=PARTUUID=614e0000-0000-4b53-8000-1d28000054a9 rw rootwait rootfstype=ext4" >> $(KERNEL_PATH)/boot_linux/extlinux/extlinux.conf
 	
-	cp -f $(KERNEL_PATH)/initramfs-toybrick-${VERSION}.img $(KERNEL_PATH)/boot_linux/
+	cp -f $(KERNEL_PATH)/initramfs-toybrick-$(VERSION).img $(KERNEL_PATH)/boot_linux/
 	cp -f $(KERNEL_PATH)/rescue.sh $(KERNEL_PATH)/boot_linux/
 	if [ "`uname -i`" == "aarch64" ]; then
 		echo y | mke2fs -b 4096 -d $(KERNEL_PATH)/boot_linux -i 8192 -t ext2 $(KERNEL_PATH)/boot_linux.img $((64 * 1024 * 1024 / 4096))
