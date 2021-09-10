ab2c37fc99d01a0147f03436b60f52b3bf433e4f
Grant Likely <grant.likely@arm.com>
2021-09-10 16:16:21 +0100

    Remove obsolete patches from build repo
    The patches for board support are now commits in the U-Boot repo instead
    of patched at build time.
    
    Signed-off-by: Grant Likely <grant.likely@arm.com>

--- TB-RK3399proD.mk
+++ TB-RK3399proD.mk
@@ -3,15 +3,3 @@ include rk3399.mk
 U-BOOT_DEFCONFIG_FILES := \
 	$(U-BOOT_PATH)/configs/toybrick-prod_defconfig \
 	$(ROOT)/build/kconfig/TB-RK3399proD.config
-
-U-BOOT_PATCHES := \
-	$(ROOT)/build/0001-CapsuleCommon.patch \
-	$(ROOT)/build/0002-Toybrick-uboot-bringup-code.patch \
-	$(ROOT)/build/0003-TBRK3399proD-DFU.patch
-
-u-boot-patch:
-	cd $(U-BOOT_PATH) && git reset --hard
-	cd $(U-BOOT_PATH) && git clean -fdx
-	cd $(U-BOOT_PATH) && git apply $(U-BOOT_PATCHES)
-
-u-boot capsule: u-boot-patch
